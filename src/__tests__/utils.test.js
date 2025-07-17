2e239c9cf6dbfc4b00edbb35ae59af8ce8505d20
alex kamau <alex.kamau1@student.moringaschool.com>
2025-07-17 14:36:39 -0700

    complete lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -2,8 +2,23 @@ import { currentAgeForBirthYear } from "../utils";
 
 describe("currentAgeForBirthYear", () => {
   it("returns the age of a person based on the year of birth", () => {
+   
     const birthYear = 1984;
-    const ageOfPerson = currentAgeForBirthYear(birthYear);
-    expect(ageOfPerson).toBe(38);
+    const expectedAge = new Date().getFullYear() - birthYear;
+
+   const result = currentAgeForBirthYear(birthYear);
+
+    expect(result).toBe(expectedAge);
+  });
+
+  it("returns the current year for a person born in year 0", () => {
+   
+    const birthYear = 0;
+    const expectedAge = new Date().getFullYear();
+
+    const result = currentAgeForBirthYear(birthYear);
+
+    expect(result).toBe(expectedAge);
   });
 });
+
